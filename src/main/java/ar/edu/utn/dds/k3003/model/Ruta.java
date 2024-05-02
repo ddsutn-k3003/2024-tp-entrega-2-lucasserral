a2a683ef44bdd8944e82638653b935d432d4840a
Lucas Serral <lucasser21@gmail.com>
2024-05-02 01:31:28 -0300

    [ADD] Ruta

--- src/main/java/ar/edu/utn/dds/k3003/model/Ruta.java
+++ src/main/java/ar/edu/utn/dds/k3003/model/Ruta.java
@@ -1,5 +1,21 @@
 package ar.edu.utn.dds.k3003.model;
 
-public class Ruta {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+public class Ruta {
+    private Long id;
+    private Long colaboradorId;
+    private Integer heladeraIdOrigen;
+    private Integer heladeraIdDestino;
+
+    public Ruta(Long colaboradorId, Integer heladeraIdOrigen, Integer heladeraIdDestino) {
+        this.colaboradorId = colaboradorId;
+        this.heladeraIdDestino = heladeraIdDestino;
+        this.heladeraIdOrigen = heladeraIdOrigen;
+    }
 }
